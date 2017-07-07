6cfc1dcf0f7a0e11c5ca0de6b0fa2c2688635ba6
Michael Trotzer <michael.trotzer@googlemail.com>
2017-07-07 11:38:01 +0200

    FOSUserBundle

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Micha
+ * Date: 03.07.2017
+ * Time: 13:26
+ */
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
